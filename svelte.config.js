796ac63ce03d681d4082605f29a3d4c59fbd306f
nightguarder <nigthguarder@gmail.com>
2023-01-27 00:10:58 +0100

    TALWIND.CSS configured and running!

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,11 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
-
+import preprocess from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter()
-  }
-
+  },
+  preprocess:[preprocess({postcss: true})]
 };
 
 export default config;
